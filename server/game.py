6a0203024fedffb72455a4ee26130e5a50f48883
Jonathan Scott <jonathan@jscott.me>
2014-03-22 17:21:10 +0000

    Place simple ships

--- server/game.py
+++ server/game.py
@@ -1,4 +1,9 @@
 """ Battleships game components. """
+from copy import copy
+
+# Useful constants
+HORIZONTAL = 0
+VERTICAL = 1
 
 # Use a 10x10 board
 BOARD_SIZE = 10
@@ -7,14 +12,51 @@ BOARD_SIZE = 10
 class Board(object):
 
     """ A game board that contains ships """
-    
+
+    def __init__(self):
+        self.ships = []
+
+    def ship_at_position(self, x, y):
+        """ Return the ship at position x, y. None if none. """
+        for ship in self.ships:
+            if [x, y] in ship["coordinates"]:
+                return ship
+        return None
+
     def current_state(self):
-        """ Return or output the state of the board. """
-        rows = []
+        """ Return the current state of the board. """
+        columns = []
         for y in range(BOARD_SIZE):
-            current_row = ""
+            current_column = []
             for x in range(BOARD_SIZE):
-                # TODO: Check if there is a ship in this position
-                current_row += " "
-            rows.append(current_row)
-        return "\n".join(rows)
+                ship = self.ship_at_position(x, y)
+                if ship:
+                    current_column.append(ship["size"])
+                else:
+                    current_column.append(None)
+            columns.append(current_column)
+        return columns
+
+    def __str__(self):
+        """ Represent the current state of the board as a string. """
+        state = self.current_state()
+        output = ""
+        for column in state:
+            for cell in column:
+                if not cell:
+                    output += " "
+            output += "\n"
+
+        return output[:-1]
+
+    def place_ship(self, size, x, y, direction=HORIZONTAL):
+        """ Place a ship sized size at x,y. """
+        coordinates = []
+        current_coordinate = [x, y]
+        current_size = size
+        while current_size > 0:
+            coordinates.append(copy(current_coordinate))
+            current_size -= 1
+            current_coordinate[direction] += 1
+
+        self.ships.append({"size": size, "coordinates": coordinates})
--- server/tests/test_ship_board.py
+++ server/tests/test_ship_board.py
@@ -1,11 +1,29 @@
 from unittest import TestCase
 from ..game import Board
 
+EMPTY_BOARD_STR = "          \n" * 9 + " " * 10
+EMPTY_BOARD = [[None]*10]*10
+
 
 class TestBoard(TestCase):
-    def test_board_displays_empty_output(self):
-        """ Test that a ship board can output an empty display. """
+    def test_board_empty_state(self):
+        """ Test the board's empty state is correct. """
         board = Board()
-        output = board.current_state()
+        self.assertEquals(board.current_state(), EMPTY_BOARD)
+
+    def test_board_displays_empty_output(self):
+        """ Test that a board can output an empty display. """
+        board = Board()
+        output = str(board)
         # 10 empty rows
-        self.assertEquals(output, "          \n" * 9 + " " * 10)
+        self.assertEquals(output, EMPTY_BOARD_STR)
+
+    def test_board_can_place_single_ship(self):
+        """ Test that board can place a ship. """
+        board = Board()
+        board.place_ship(1, 0, 0)
+        self.assertEquals(board.ships[0]["size"], 1)
+        self.assertEquals(board.ships[0]["coordinates"], [[0, 0]])
+
+        state = board.current_state()
+        self.assertEquals(state[0][0], 1)
